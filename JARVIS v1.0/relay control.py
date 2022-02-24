0a8d28cdda82266f9480b6bec9463c6930356012
AKSHAT <46858110+akshatsingh16@users.noreply.github.com>
2022-02-24 23:49:29 +0530

    Update relay control.py

--- JARVIS v1.0/relay control.py
+++ JARVIS v1.0/relay control.py
@@ -1,27 +1,27 @@
-# from os import write
-# import serial
+ from os import write
+ import serial
 
-# Arduino_Serial = serial.Serial('com12', 9600)
+ Arduino_Serial = serial.Serial('com12', 9600)
 
-# while True:
-#     Arduino_Serial.write(str.encode('on1'))
-#     print("relay1on")
+ while True:
+     Arduino_Serial.write(str.encode('on1'))
+     print("relay1on")
 
-#     input_data = input()
+     input_data = input()
 
-#     if(input_data == 'on1'):
-#         Arduino_Serial.write(str.encode('on1'))
-#         print("relay1on")
+     if(input_data == 'on1'):
+         Arduino_Serial.write(str.encode('on1'))
+         print("relay1on")
 
-#     if(input_data == 'off1'):
-#         Arduino_Serial.write(str.encode('off1'))
-#         print("relay1off")
+     if(input_data == 'off1'):
+         Arduino_Serial.write(str.encode('off1'))
+         print("relay1off")
 
-#     if(input_data == 'on2'):
-#         Arduino_Serial.write(str.encode('on2'))
-#         print("relay1on")
+     if(input_data == 'on2'):
+         Arduino_Serial.write(str.encode('on2'))
+         print("relay1on")
 
-#     if(input_data == 'off2'):
-#         Arduino_Serial.write(str.encode('off2'))
-#         print("relay1off")
-#     break
+     if(input_data == 'off2'):
+         Arduino_Serial.write(str.encode('off2'))
+         print("relay1off")
+     break
